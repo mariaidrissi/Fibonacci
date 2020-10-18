2ae5e65b8678d16b39355ea147d54a52367b0fbf
Maria Idrissi <idrissimaria2@gmail.com>
2020-10-18 10:38:32 +0200

    cleansed

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM ubuntu:latest
 RUN apt update -y
-RUN apt install -y python3-pip python3-dev build-essential python3-tk
+RUN apt install -y python3-pip python3-dev build-essential
 
 WORKDIR /app
 COPY requirements.txt /app
 RUN pip3 install -r requirements.txt
 
-COPY app.py /app
+COPY app.py back.py /app/
 
 EXPOSE 5000
 ENTRYPOINT ["python3"]
--- app.py
+++ app.py
@@ -1,50 +1,13 @@
 #!flask/bin/python3
 from flask import Flask, jsonify, abort, make_response
-# from flask_caching import Cache
-# import tkinter as tk
-# from tkinter import *
-import requests
 
-# interface=Tk()
-# texte=Label(interface, text='API REST Fibonacci !', fg='red')
-# texte.pack()
-# entree = tk.Entry(interface, width=30)
-# entree.pack()
-#
-#
-# def fibo_tk():
-#     nb = str(int(entree.get()))
-#     print(nb)
-#     response = requests.get("http://0.0.0.0:5000/fibonacci/api/v1.0/nombres/"+ nb)
-#     print(response)
-#     rep = json.loads(response.content)
-#     print(rep)
-#     variable_modifiable.set("Result : "+ str(rep["Fibonacci"]))
-
-# boutton = Button(interface, text = 'Calculer nombre fibonacci', command = fibo_tk)
-# boutton.pack()
-# boutton2=Button(interface, text='Quitter', command=interface.destroy)
-# boutton2.pack()
-# interface.mainloop()
+import back
 
 app = Flask(__name__)
-#cache = Cache(app, config={'CACHE_TYPE': 'simple'})
-#cache.init_app(app)
 
 @app.route('/fibonacci/api/v1.0/nombres/<int:nb>', methods=['GET'])
 def web_fibonacci(nb):
-    #NombreFibo=fibonacci(nb)
-    #cache.set(str(nb),NombreFibo)
-    #r=cache.get("nb")
-    #for k in cache.cache._cache:
-    #    print(k, cache.get(k))
-    return jsonify({'Fibonacci': fibonacci(nb)})
-
-def fibonacci(nb):
-    if(nb <= 1):
-            return nb
-    else:
-            return (fibonacci(nb-1) + fibonacci(nb-2))
+    return jsonify({'Fibonacci': back.fibonacci(nb)})
 
 @app.errorhandler(404)
 def not_found(error):
